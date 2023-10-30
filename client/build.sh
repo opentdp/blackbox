80b29206e1b37e54881b94d7afee3c244fd4e61f
若海 <wang@rehiy.com>
2023-10-30 09:12:21 +0800

    chore: 指定编译平台

--- client/build.sh
+++ client/build.sh
@@ -9,19 +9,49 @@ set -o noglob
 export CGO_ENABLED=0
 export GO111MODULE=on
 
-export os_all="linux windows darwin freebsd"
-export arch_all="386 amd64 arm arm64 mips64 mips64le mips mipsle riscv64"
+build() {
+    echo building for $1/$2
+    target=build/tdp-blackbox-$1-$2
+    if [ x"$1" = x"windows" ]; then
+        target="${target}.exe"
+    fi
+    GOOS=$1 GOARCH=$2 go build -ldflags="-s -w" -o $target main.go
+}
 
-for os in $os_all; do
-    for arch in $arch_all; do
-        echo building for $os/$arch
-        target=build/tdp-blackbox-$os-$arch
-        if [ x"$os" = x"windows" ]; then
-            target="${target}.exe"
-        fi
-        GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o $target main.go
-    done
-done
+####################################################################
+
+build android arm64
+
+build darwin amd64
+build darwin arm64
+
+build freebsd 386
+build freebsd amd64
+build freebsd arm64
+
+build linux 386
+build linux amd64
+build linux armv7
+build linux arm64
+build linux mips
+build linux mipsle
+build linux mips64
+build linux mips64le
+build linux ppc64
+build linux ppc64le
+build linux s390x
+
+build netbsd 386
+build netbsd amd64
+build netbsd arm64
+
+build openbsd 386
+build openbsd amd64
+build openbsd arm64
+
+build windows 386
+build windows amd64
+build windows arm64
 
 ####################################################################
 
