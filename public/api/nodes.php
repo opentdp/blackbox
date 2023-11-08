a5053ebdf5fa2920b2ea6e30c92cf720f3ba5a6e
若海 <wang@rehiy.com>
2023-11-08 09:19:34 +0800

    feat: 增加配置信息

--- public/api/nodes.php
+++ public/api/nodes.php
@@ -1,8 +1,8 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json;charset=utf-8');
 
 include dirname(__DIR__) . '/vendor.php';
