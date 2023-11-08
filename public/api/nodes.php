c98d16abceb027056343a815588af1654556fb62
若海 <wang@rehiy.com>
2023-11-08 15:44:28 +0800

    feat: 接口支持yaml格式

--- public/api/nodes.php
+++ public/api/nodes.php
@@ -1,19 +1,28 @@
 <?php
 
+$format = 'json';
+if (isset($_GET['format']) && in_array($_GET['format'], ['yaml'])) {
+    $format = $_GET['format'];
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json;charset=utf-8');
-
-include dirname(__DIR__) . '/vendor.php';
+header('Content-Type: application/' . $format . ';charset=utf-8');
 
 // fetch nodes
 
+include dirname(__DIR__) . '/vendor.php';
+
 if (!$proxies = get_nodes($_GET['name'] ?? '')) {
-    exit(json_encode(['error' => 'not found']));
+    if ($format == 'json') {
+        exit(json_encode(['error' => 'not found']));
+        exit;
+    }
+    exit('error: not found');
 }
 
-$output = [];
+$nodes = [];
 foreach ($proxies as &$node) {
     $config = [
         'name' => $node['name'],
@@ -27,11 +36,28 @@ foreach ($proxies as &$node) {
     $config_file = dirname(__DIR__) . '/nodes/' . $node['name'] . '.php';
     if (is_file($config_file)) {
         $config = array_merge($config, include($config_file));
-        $output[$node['name']] = $config;
+        $nodes[$node['name']] = $config;
         touch($config_file);
     }
 }
 
-ksort($output);
+ksort($nodes);
 
-echo json_encode($output, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+// output
+
+if ($format == 'json') {
+    echo json_encode($nodes, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    exit;
+}
+
+if ($format == 'yaml') {
+    $output = [];
+    foreach ($nodes as $name => $node) {
+        $output[] = "$name:";
+        foreach ($node as $key => $value) {
+            $output[] = "  $key: $value";
+        }
+    }
+    echo implode("\n", $output);
+    exit;
+}
--- public/api/sites.php
+++ public/api/sites.php
@@ -1,18 +1,48 @@
 <?php
 
+$format = 'json';
+if (isset($_GET['format']) && in_array($_GET['format'], ['yaml'])) {
+    $format = $_GET['format'];
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json;charset=utf-8');
+header('Content-Type: application/' . $format . ';charset=utf-8');
 
 include dirname(__DIR__) . '/vendor.php';
 
 // fetch sites
 
 if (!$sites = get_sites($_GET['name'] ?? '')) {
-    exit(json_encode(['error' => 'not found']));
+    if ($format == 'json') {
+        exit(json_encode(['error' => 'not found']));
+        exit;
+    }
+    exit('error: not found');
 }
 
 ksort($sites);
 
-echo json_encode($sites, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+// output
+
+if ($format == 'json') {
+    echo json_encode($sites, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    exit;
+}
+
+if ($format == 'yaml') {
+    $output = [];
+    foreach ($sites as &$site) {
+        $output[] = "- targets:";
+        foreach ($site['targets'] as $url) {
+            $output[] = "    - $url";
+        }
+        $output[] = "  labels:";
+        foreach ($site['labels'] as $key => $value) {
+            $output[] = "    $key: $value";
+        }
+    }
+    echo implode("\n", $output);
+    exit;
+}
